a01c2b849e58b8c359be28a4c01f0d2fffacfc37
Laboratório de Redes <labredes@exemplo.portoalegre.pucrsnet.br>
2019-08-26 15:04:08 -0300

    t1 startserver funfando

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+#rede
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+#StartServer
+cp $BASE_DIR/../custom-scripts/StartServer $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/StartServer
+
+#my_server.py
+cp $BASE_DIR/../custom-scripts/my_server.py $BASE_DIR/target/usr
+chmod +x $BASE_DIR/target/usr/my_server.py
